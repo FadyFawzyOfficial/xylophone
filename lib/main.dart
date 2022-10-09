6315acb995cfcb398030972ae3d82fce65faefa4
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-09 22:27:38 +0200

    2. Play Multiple Sounds (How to?)

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,43 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () =>
-                  AudioPlayer().play(AssetSource('audios/note1.wav')),
-              child: const Text('Click Me!'),
-            ),
+          child: Column(
+            children: [
+              XylophoneKey(soundNumber: 1, color: Colors.red),
+              XylophoneKey(soundNumber: 2, color: Colors.orange),
+              XylophoneKey(soundNumber: 3, color: Colors.yellow),
+              XylophoneKey(soundNumber: 4, color: Colors.green),
+              XylophoneKey(soundNumber: 5, color: Colors.teal),
+              XylophoneKey(soundNumber: 6, color: Colors.blue),
+              XylophoneKey(soundNumber: 7, color: Colors.purple),
+            ],
           ),
         ),
       ),
     );
   }
+}
+
+class XylophoneKey extends StatelessWidget {
+  final int soundNumber;
+  final Color color;
+  final AudioPlayer _player = AudioPlayer();
+
+  XylophoneKey({
+    Key? key,
+    required this.soundNumber,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: null,
+      style: ElevatedButton.styleFrom(backgroundColor: color),
+      onPressed: () => _playSound(soundNumber),
+    );
+  }
+
+  void _playSound(int soundNumber) =>
+      _player.play(AssetSource('audios/note$soundNumber.wav'));
 }
